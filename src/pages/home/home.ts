6b237874fccd189f37e5717aa16a27520d64dccb
LucNieto <noodle3030@hotmail.com>
2018-10-07 12:32:51 -0500

    Iniciando proyecto y probando navegación con un botón

---
+++ src/pages/home/home.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { LugarPage } from '../lugar/lugar';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+
+
+  NavegarALugar(){
+    this.navCtrl.push(LugarPage);
+  }
+
+} 
